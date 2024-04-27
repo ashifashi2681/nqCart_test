4c1211129a86fb14517a1b3cfac6dca2cc0727b4
ashifashi2681 <143752870+ashifashi2681@users.noreply.github.com>
2024-04-27 10:12:20 +0530

    Add files via upload

--- scripts/product-page.js
+++ scripts/product-page.js
@@ -5,3 +5,25 @@ const breadCrumb = document.querySelector(".breadcrumb");
 const navHeight = navBar.offsetHeight - -15;
 
 breadCrumb.style.marginTop = navHeight + "px";
+
+/* product chart size section */
+
+const sizeText = document.querySelector(".product-page-size-chart div span")
+const sizeButtons = document.querySelectorAll(".product-size-chart button");
+
+sizeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        sizeText.innerText = button.innerText;
+        button.classList.add('highlight')
+
+        sizeButtons.forEach((otherButton) => {
+            if (otherButton !== button) {
+                otherButton.classList.remove('highlight')
+            }
+        });
+    });
+});
+
+
+
+
