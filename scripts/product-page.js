5c55e716c10bec1c0b93e4257528329e2013612c
ashifashi2681 <143752870+ashifashi2681@users.noreply.github.com>
2024-04-29 21:02:53 +0530

    Add files via upload

--- scripts/product-page.js
+++ scripts/product-page.js
@@ -1,27 +1,26 @@
 const navBar = document.getElementById("nav-bar");
 const breadCrumb = document.querySelector(".breadcrumb");
 
-
 const navHeight = navBar.offsetHeight - -15;
 
 breadCrumb.style.marginTop = navHeight + "px";
 
 /* product chart size section */
 
-const sizeText = document.querySelector(".product-page-size-chart div span")
+const sizeText = document.querySelector(".product-page-size-chart div span");
 const sizeButtons = document.querySelectorAll(".product-size-chart button");
 
 sizeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        sizeText.innerText = button.innerText;
-        button.classList.add('highlight')
+	button.addEventListener("click", () => {
+		sizeText.innerText = button.innerText;
+		button.classList.add("highlight");
 
-        sizeButtons.forEach((otherButton) => {
-            if (otherButton !== button) {
-                otherButton.classList.remove('highlight')
-            }
-        });
-    });
+		sizeButtons.forEach((otherButton) => {
+			if (otherButton !== button) {
+				otherButton.classList.remove("highlight");
+			}
+		});
+	});
 });
 
 /* ---------------------------------------------------------------------------------------------------------- */
@@ -31,7 +30,7 @@ sizeButtons.forEach((button) => {
 var swiper = new Swiper(".mySwiperThumb", {
 	loop: true,
 	spaceBetween: 15,
-	slidesPerView: 'auto',
+	slidesPerView: "auto",
 	freeMode: true,
 	watchSlidesProgress: true,
 });
@@ -47,30 +46,53 @@ var swiper2 = new Swiper(".mySwiper2", {
 	},
 });
 
-/* ------------------------------------------------------------------------------------------- */
+/* product-page-freaquently-bought-together slider */
+
+var swiper = new Swiper(".mySwiperBoughtTogether", {
+	slidesPerView: "auto",
+	spaceBetween: 30,
+});
+
+/* freaquently-bought-together checkbox */
+const checkbox = document.querySelectorAll(
+	".product-page-freaquently-bought-together-card input"
+);
+
+const productCard = document.querySelectorAll(
+	".product-page-freaquently-bought-together-card"
+);
+
+productCard.forEach((productCard, index) => {		
+	productCard.addEventListener("click", () => {	
+		if (checkbox[index].checked) {
+			checkbox[index].checked = false;
+		} else {
+			checkbox[index].checked = true;
+		} 
+	});	
+});
+
 /* user review images section */
 var swiper = new Swiper(".userImageSlider", {
 	freeMode: true,
 	spaceBetween: 15,
-	slidesPerView:'auto'
+	slidesPerView: "auto",
 });
 
 /* --------------------------------------------------------- */
 
 /* review questions search */
 const searchIcon = document.querySelector(".product-page-question-search img");
-const searchInput = document.querySelector(".product-page-question-search input");
+const searchInput = document.querySelector(
+	".product-page-question-search input"
+);
 
 searchIcon.addEventListener("click", () => {
 	searchInput.classList.add("show");
-	
-	
+
 	document.addEventListener("click", (e) => {
 		if (!searchIcon.contains(e.target) && !searchInput.contains(e.target)) {
 			searchInput.classList.remove("show");
 		}
 	});
-	
-	
 });
-
