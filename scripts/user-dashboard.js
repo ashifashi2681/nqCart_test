c11ac65f5b5457f8de56dbb0df55b38f10067392
ashifashi2681 <143752870+ashifashi2681@users.noreply.github.com>
2024-08-15 21:33:06 +0530

    profile section added

---
+++ scripts/user-dashboard.js
@@ -0,0 +1,41 @@
+/* toggling user dashboard sections class */
+
+const profileBtns = document.querySelectorAll(".user-dashboard-menu > button");
+const userDashboardSections = document.querySelectorAll(
+	"#user-dashboard-right-section"
+);
+
+profileBtns.forEach((btn, index) => {
+	btn.addEventListener("click", () => {
+		if (index === 4) {
+			return;
+		}
+
+		userDashboardSections.forEach((dashboard) => {
+			dashboard.classList.remove("active");
+		});
+		profileBtns.forEach((btn) => {
+			btn.classList.remove("btnActive");
+		});
+
+		userDashboardSections[index].classList.add("active");
+		btn.classList.add("btnActive");
+		profileBtns.forEach((bt) => {
+			bt.addEventListener("click", () => {
+				if (bt !== btn) {
+					userDashboardSections[index].classList.remove("active");
+					btn.classList.remove("btnActive");
+				}
+			});
+		});
+	});
+});
+
+/* logout */
+const logout = document.getElementById("logout");
+
+logout.addEventListener("click", () => {
+	// console.log("logout clicked")
+	
+});
+
